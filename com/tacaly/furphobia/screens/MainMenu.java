886891d91c4966dc17c4e3595175f5f97159866d
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2022-03-04 22:49:04 +0100

    menu

--- com/tacaly/furphobia/screens/MainMenu.java
+++ com/tacaly/furphobia/screens/MainMenu.java
@@ -1,5 +1,36 @@
 package screens;
 
-public class MainMenu {
+import com.tacaly.furphobia.Image;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.LinearGradient;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
 
+import static java.awt.SystemColor.menu;
+
+public class MainMenu {
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setScene(new Scene(createMenu()));
+        stage.show();
+    }
+
+    private Parent createMenu() {
+        Pane root = new Pane();
+        root.setPrefSize(1280,720);
+
+        Image bgImage = new Image(
+                getClass().getResource(resources/menu.jpg),
+                1280,720,
+                false, true
+        );
+        return root;
+    }
+    private static class MenuItem extends StackPane {
+        MenuItem(String name, Runnable action) {
+            LinearGradient gradient = new LinearGradient();
+        }
+    }
 }
